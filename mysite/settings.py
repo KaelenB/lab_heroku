503308cc7446c7e0d431229e7cb5fa342a59f04f
KaelenB <ktbrown@ualberta.ca>
2023-10-22 10:30:31 -0600

    'update'

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import django_on_heroku # top of the file
+import django_heroku
 from os import path
 from pathlib import Path
 
@@ -127,4 +127,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
