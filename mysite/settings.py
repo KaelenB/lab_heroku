ae5b45a1d673c60c8f21cf4a4c04b32930d9d8ef
KaelenB <ktbrown@ualberta.ca>
2023-10-23 05:47:44 -0600

    'IDK'

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4mka(*_u3&kh_v=zc4ow(_@l^_9^pc&(c$%jy0+5l(&qt=4ckv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
